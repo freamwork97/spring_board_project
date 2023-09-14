c4732b69266cd29b9f7a59a91776e3404aede98c
freamwork97 <jsy11123@naver.com>
2023-09-14 09:47:09 +0900

    paging_search

--- src/main/java/com/icia/board/controller/BoardController.java
+++ src/main/java/com/icia/board/controller/BoardController.java
@@ -38,32 +38,49 @@ public class BoardController {
     // /board/list?page=1
     @GetMapping("/list")
     public String findAll(@RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                          @RequestParam(value = "q", required = false, defaultValue = "") String q,
+                          @RequestParam(value = "type", required = false, defaultValue = "boardTitle") String type,
                           Model model) {
-        List<BoardDTO> boardDTOList = boardService.pagingList(page);
-        System.out.println("boardDTOList = " + boardDTOList);
-        model.addAttribute("boardList", boardDTOList);
+        // 검색이든 아니든 필요한 정보 : boardList, paging 전역변수 선언
+        List<BoardDTO> boardDTOList = null;
+        PageDTO pageDTO = null;
 
-        PageDTO pageDTO = boardService.pageNumber(page);
+        // 검색 요청인가 아닌가 구분
+        if (q.equals("")) {
+            //일반 페이지 요청
+            boardDTOList = boardService.pagingList(page);
+            pageDTO = boardService.pageNumber(page);
+        } else {
+            // 검색 결과 페이지 요청
+            boardDTOList = boardService.searchList(q, type, page);
+
+            pageDTO = boardService.searchPageNumber(q, type, page);
+        }
+//        System.out.println("boardDTOList = " + boardDTOList);
+        model.addAttribute("boardList", boardDTOList);
         model.addAttribute("paging", pageDTO);
+        model.addAttribute("q", q);
+        model.addAttribute("type", type);
+        model.addAttribute("page", page);
         return "boardPages/boardList";
     }
 
-    @GetMapping("/search")
-    public String search(@RequestParam("q") String q,
-                         @RequestParam("type") String type,
-                         @RequestParam(value = "page", required = false, defaultValue = "1") int page, // 검색결과에 대한 페이지
-                         Model model) {
-        List<BoardDTO> boardDTOList = boardService.searchList(q, type, page);
-        model.addAttribute("boardList", boardDTOList);
 
-        PageDTO pageDTO = boardService.searchPageNumber(q, type, page);
-        model.addAttribute("paging",pageDTO);
-        return "boardPages/boardList";
-    }
+//    @GetMapping("/search")
+//    public String search(@RequestParam("q") String q,
+//                         @RequestParam("type") String type,
+//                         @RequestParam(value = "page", required = false, defaultValue = "1") int page, // 검색결과에 대한 페이지
+//                         Model model) {
+//        model.addAttribute("boardList", boardDTOList);
+//        model.addAttribute("paging", pageDTO);
+//        return "boardPages/boardList";
+//    }
 
     @GetMapping
     public String findById(@RequestParam("id") Long id,
                            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                           @RequestParam(value = "q", required = false, defaultValue = "") String q,
+                           @RequestParam(value = "type", required = false, defaultValue = "boardTitle") String type,
                            Model model) {
         // 조회수 처리
         // 데이터 가져오기
@@ -83,6 +100,8 @@ public class BoardController {
             model.addAttribute("commentList", commentDTOList);
         }
 
+        model.addAttribute("q", q);
+        model.addAttribute("type", type);
         model.addAttribute("page", page);
         return "boardPages/boardDetail";
     }
